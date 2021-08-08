314dfebac7e914183a85ebbd7fb1ed633c2b4d7a
Changyun Go <85783423+changyun-go@users.noreply.github.com>
2021-08-08 17:13:39 +0900

    수박수박수박수박수박수?
    https://programmers.co.kr/learn/courses/30/lessons/12922?language=javascript

--- level1_watermelon.js
+++ level1_watermelon.js
@@ -1,5 +1,5 @@
 function solution(n) {
-    const array = "수박".repeat(n/2+1) 
-    const answer = array.slice(0,n);
+    const string = "수박".repeat(n/2+1) 
+    const answer = string.slice(0,n);
     return answer;
 }
